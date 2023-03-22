bccd927464376347a4ca095f1ab30fe6a0cb364f
leo.ZSLong <1542123803@qq.com>
2023-03-22 23:15:02 +0800

    elasticsearch index operations

--- elasticsearch-rest-template/src/test/java/com/leo/service/study/ElasticsearchRestTemplateApplicationTests.java
+++ elasticsearch-rest-template/src/test/java/com/leo/service/study/ElasticsearchRestTemplateApplicationTests.java
@@ -1,27 +1,16 @@
 package com.leo.service.study;
 
 import com.leo.service.study.elasticsearch.domain.Product;
+import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 
-@SpringBootTest(classes = ElasticsearchRestTemplateApplication.class)
+@SpringBootTest
 @Slf4j
 class ElasticsearchRestTemplateApplicationTests {
-
-
-    @Autowired
-    public ElasticsearchRestTemplate elasticsearchRestTemplate;
-
-
-    @Test
-    public void findById() {
-        Product product = elasticsearchRestTemplate.get("HzwfMIUBnhBSI8p_vYdc", Product.class);
-        log.info("product:{}", product);
-    }
-
 
 
 }
--- elasticsearch-rest-template/src/test/java/com/leo/service/study/IndexOperationsTests.java
+++ elasticsearch-rest-template/src/test/java/com/leo/service/study/IndexOperationsTests.java
@@ -5,15 +5,22 @@
 
 package com.leo.service.study;
 
+import com.alibaba.fastjson.JSONObject;
+import com.leo.service.study.elasticsearch.domain.Headmaster;
 import com.leo.service.study.elasticsearch.domain.Student;
-import com.leo.service.study.elasticsearch.repository.ProductDao;
+import com.leo.service.study.elasticsearch.domain.Teacher;
+import java.util.List;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.IndexInformation;
 import org.springframework.data.elasticsearch.core.IndexOperations;
 import org.springframework.data.elasticsearch.core.document.Document;
+import org.springframework.data.elasticsearch.core.index.Settings;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 
 /**
  * TODO
@@ -28,22 +35,53 @@ public class IndexOperationsTests {
     @Autowired
     public ElasticsearchRestTemplate elasticsearchRestTemplate;
 
-    @Autowired
-    public ProductDao productDao;
-
+    /**
+     * 创建索引
+     * @author Leo
+     */
     @Test
     public void indexCreate() {
-        // Student build = Student.builder()
-        //     .age(27)
-        //     .sex("man")
-        //     .name("leo").headPictureUrl("www.image.com").build();
-        // elasticsearchRestTemplate.save(build);
-        IndexOperations indexOperations = elasticsearchRestTemplate.indexOps(Student.class);
-        indexOperations.create();
-
-        productDao.findById("");
-
-        Document mapping = indexOperations.createMapping();
-
+        commonIndexCreate(Student.class);
+        commonIndexCreate(Headmaster.class);
+        commonIndexCreate(Teacher.class);
     }
+    private <T> void commonIndexCreate(Class<T> tClass) {
+        IndexOperations indexOperations = elasticsearchRestTemplate.indexOps(tClass);
+        // 创建索引
+        boolean b = indexOperations.create();
+        boolean b1 = false;
+        if (b) {
+            // 创建mapping
+            Document mapping = indexOperations.createMapping();
+            b1 = indexOperations.putMapping(mapping);
+        }
+        log.info("创建索引及mapping结果:{}", b && b1);
+    }
+
+
+    /**
+     * 删除索引
+     * @author Leo
+     */
+    @Test
+    public void deleteIndex() {
+        IndexCoordinates of = IndexCoordinates.of("studnet_new1");
+        IndexOperations indexOperations = elasticsearchRestTemplate.indexOps(of);
+        boolean delete = indexOperations.delete();
+        log.info("删除索引结果：{}", delete);
+    }
+
+    @Test
+    public void searchIndexInfo() {
+        IndexCoordinates of = IndexCoordinates.of("student_new1");
+        IndexOperations indexOperations = elasticsearchRestTemplate.indexOps(of);
+        List<IndexInformation> information = indexOperations.getInformation();
+        Map<String, Object> mapping = indexOperations.getMapping();
+        Settings settings = indexOperations.getSettings();
+        log.info("indexInfo：{}", JSONObject.toJSONString(information));
+        log.info("mappings：{}", mapping);
+        log.info("settings：{}", settings);
+    }
+
+
 }
